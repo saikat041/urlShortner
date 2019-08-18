479dfa7e6ead7ae1fca3cf3778cb0d1c48c22c59
saikat <saikat.dakshana15@gmail.com>
2019-08-18 21:45:32 +0530

    Added database and improved UI

--- app.js
+++ app.js
@@ -1,37 +1,71 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const sha1 = require('sha1');
+const MongoClient = require('mongodb').MongoClient;
+const shortid = require("shortid");
+const validUrl = require('valid-url');
 
-const baseUrl = "http://localhost:3000"
-const PORT = 3000; 
-const map = {}
+// Configuration
+const { PORT, BASE_URL, DB_URL} = process.env;
+if(!(PORT && BASE_URL && DB_URL)){
+    throw new Error("Define environment variables properly");
+}
+
+var myDb;
+
+// Database connection
+MongoClient.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
+    if (err) throw err;
+    myDb = db.db('urlshortner');
+  });
+
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
+
 
 // Showing home page
 app.get('/', function(req, res){
    res.sendFile(__dirname + '/public/index.html');
 });
 
+
 // API for getting shorter url of input url
-app.post('/api/getShortURL', function(req, res){
-    var longUrl = req.body.longUrl;
-    var key = sha1(longUrl);
-    map[key] = longUrl;
-    res.json(baseUrl + '/' + key);
+app.post('/api/getShortURL', async function(req, res) {
+    var { longUrl } = req.body;
+
+    if(!validUrl.isUri){
+        res.json({error : "Invalid URL"});
+    }
+
+    var result  = await myDb.collection("urlMapping").find({longUrl}).toArray();
+    if(result.length === 0){
+        let key = shortid.generate();
+        let shortUrl = BASE_URL + '/' + key;
+        let obj = {longUrl, shortUrl}
+        await myDb.collection('urlMapping').insertOne(obj);
+        result.push(obj);
+    }
+
+    res.json({ shortUrl : result[0].shortUrl });
 });
 
 
 // Redirection happens here.
-app.get('/:key', function(req, res){
+app.get('/:key', async function(req, res){
     var key = req.params["key"];
-    if(key in map) {
-        res.redirect(map[key]);
-        return;
+    var shortUrl = BASE_URL + '/' + key;
+    var result = await myDb.collection('urlMapping').find({shortUrl}).toArray();
+    if(result.length === 0){
+        res.status(404).send("Not found");
     }
-    res.status(404).send("Not found");
+    var { longUrl } = result[0];
+
+    if( !(longUrl.startsWith("http://") || longUrl.startsWith('https://')) ) {
+        longUrl = 'http://' + longUrl;
+    }
+
+    res.redirect(longUrl);    
 });
 
 
--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 function create(){
     var longUrl = document.getElementById("longUrl").value;
-    var url = 'http://localhost:3000/api/getShortURL';
+    var url = "/api/getShortURL";
 
     var body = JSON.stringify({ longUrl : longUrl });
     var headers = new Headers({ 'Content-Type' : 'application/json' });
@@ -8,7 +8,13 @@ function create(){
     fetch(url, {method : 'POST', body : body, headers : headers})
     .then(res => res.json())
     .then(function(res){
-        document.getElementById("output").innerHTML = res;
+        document.getElementById("output").innerHTML = res.shortUrl;
+        document.getElementById("link").setAttribute("href", res.shortUrl);
     });
 
+}
+
+function copyShortUrl(){
+  var shortUrl = document.getElementById("output").innerText;
+  navigator.clipboard.writeText(shortUrl);
 }
